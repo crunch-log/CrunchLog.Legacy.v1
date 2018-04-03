732e6abd622750fb4e5d7edd27a737be846fdf2e
Ishan Jain <ishan.jain@avensia.com>
2018-04-03 13:45:12 +0200

    Add copy capability to DirectoryInfo

--- src/Bit0.CrunchLog/Extensions/DirectoryInfoExtensions.cs
+++ src/Bit0.CrunchLog/Extensions/DirectoryInfoExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using Markdig.Extensions.DefinitionLists;
 
 namespace Bit0.CrunchLog.Extensions
 {
@@ -25,6 +26,42 @@ namespace Bit0.CrunchLog.Extensions
         public static String NormalizePath(this String path)
         {
             return path.Replace('\\', '/').Replace('/', Path.DirectorySeparatorChar);
+        }
+
+        
+        public static void Copy(this DirectoryInfo dir, DirectoryInfo destDir, Boolean copySubDirs = true)
+        {
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + dir.FullName);
+            }
+
+            var dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!destDir.Exists)
+            {
+                destDir.Create();
+            }
+        
+            // Get the files in the directory and copy them to the new location.
+            var files = dir.GetFiles();
+            foreach (var file in files)
+            {
+                var temppath = destDir.CombinePath(file.Name);
+                file.CopyTo(temppath, true);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (var subdir in dirs)
+                {
+                    var temppath = new DirectoryInfo(destDir.CombinePath(subdir.Name));
+                    subdir.Copy(temppath, copySubDirs);
+                }
+            }
         }
     }
 }

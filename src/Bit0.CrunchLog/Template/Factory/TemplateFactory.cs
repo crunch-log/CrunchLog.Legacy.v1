79192e4e45250ec0790e47f8463ec5f900bf70bf
b1thunt3r <ishan@jain.se>
2018-11-29 16:15:59 +0000

    Stream line TemplateFactory constructor

--- src/Bit0.CrunchLog/Template/Factory/TemplateFactory.cs
+++ src/Bit0.CrunchLog/Template/Factory/TemplateFactory.cs
@@ -1,8 +1,5 @@
 ﻿using Bit0.CrunchLog.Config;
-using Bit0.CrunchLog.Extensions;
 using Bit0.CrunchLog.Template.Models;
-using Newtonsoft.Json;
-using System.Linq;
 
 namespace Bit0.CrunchLog.Template.Factory
 {
@@ -23,24 +20,9 @@ namespace Bit0.CrunchLog.Template.Factory
             Theme = SiteConfig.Theme;
             Engine = templateEngine;
 
-            InitOutput();
-        }
-
-        private void InitOutput ()
-        {
             if (!SiteConfig.Paths.OutputPath.Exists)
             {
                 SiteConfig.Paths.OutputPath.Create();
-            }
-
-            foreach (var dir in Theme.Assets.Directories.Select(d => Theme.Directory.CombineDirPath(d)))
-            {
-                dir.Copy(SiteConfig.Paths.OutputPath.CombineDirPath(dir.Name));
-            }
-
-            foreach (var file in Theme.Assets.Files.Select(f => Theme.Directory.CombineFilePath(f)))
-            {
-                file.CopyTo(SiteConfig.Paths.OutputPath.CombineDirPath(file.Name).FullName, true);
             }
         }
 

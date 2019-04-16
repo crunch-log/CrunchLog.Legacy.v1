fe9804c07277892fdd2d07e2db964b5de67a73cf
b1thunt3r <ishan@jain.se>
2019-04-16 09:47:03 +0100

    Replace EmbetIO with Kestrel

--- src/Bit0.CrunchLog.Cli/RunCommand.cs
+++ src/Bit0.CrunchLog.Cli/RunCommand.cs
@@ -1,12 +1,11 @@
 ﻿using Bit0.CrunchLog.Cli.Extensions;
 using McMaster.Extensions.CommandLineUtils;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Threading;
-using System.Threading.Tasks;
-using Unosquare.Labs.EmbedIO;
-using Unosquare.Swan;
 
 namespace Bit0.CrunchLog.Cli
 {
@@ -34,76 +33,24 @@ namespace Bit0.CrunchLog.Cli
                 generator.CleanOutput();
                 generator.Publish();
 
-                Terminal.OnLogMessageReceived += (s, e) =>
-                {
-                    LogLevel level;
-
-                    switch (e.MessageType)
-                    {
-                        case LogMessageType.None:
-                            level = LogLevel.None;
-                            break;
-                        case LogMessageType.Info:
-                            level = LogLevel.Information;
-                            break;
-                        case LogMessageType.Debug:
-                            level = LogLevel.Debug;
-                            break;
-                        case LogMessageType.Trace:
-                            level = LogLevel.Trace;
-                            break;
-                        case LogMessageType.Error:
-                            level = LogLevel.Error;
-                            break;
-                        case LogMessageType.Warning:
-                            level = LogLevel.Warning;
-                            break;
-                        case LogMessageType.Fatal:
-                            level = LogLevel.Critical;
-                            break;
-                        default:
-                            throw new ArgumentOutOfRangeException();
-                    }
-
-                    logger.Log(level, new EventId(2001, e.Source), $"[{e.Source}] {e.Message}", e.Exception, (state, exception) => state.ToString());
-                };
-                Terminal.Settings.DisplayLoggingMessageType = LogMessageType.None;
-
-                var server = WebServer
-                    .Create(args.Url)
-                    .EnableCors()
-                    .WithLocalSession()
-                    .WithStaticFolderAt(config.Paths.OutputPath.FullName);
-
-                server.OnNotFound = (c) =>
-                {
-                    logger.Log(LogLevel.Error, new EventId(2002, "Not Found"), $"Cannot find {c.Request.Url}");
-
-                    return Task.FromResult(true);
-                };
-
-                var cts = new CancellationTokenSource();
-                var task = server.RunAsync(cts.Token);
+                var host = new WebHostBuilder()
+                    .UseKestrel()
+                    .UseWebRoot(config.Paths.OutputPath.FullName)
+                    .Configure(x => x
+                                        .UseFileServer()
+                                        .UseDirectoryBrowser()
+                                        .UseStatusCodePages()
+                    )
+                    .UseUrls(args.Url)
+                    .ConfigureLogging(logging => logging
+                                                    .ClearProviders()
+                                                    .AddConsole()
+                    )
+                    .Build();
+                var task = host.RunAsync();
 
                 app.OpenBrowser(args.Url);
-
-                Console.WriteLine("Press any key to close server.");
-                Console.ReadKey(true);
-                cts.Cancel();
-
-                try
-                {
-                    task.Wait(cts.Token);
-                }
-                catch (AggregateException)
-                {
-                    // We'd also actually verify the exception cause was that the task
-                    // was canceled.
-                }
-                finally
-                {
-                    cts.Dispose();
-                }
+                task.Wait();
             });
         }
     }

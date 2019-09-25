49b594753359d9d571959da441cd45963791a252
b1thunt3r <ishan@jain.se>
2019-09-25 16:35:48 +0100

    Check if src exists in theme.

--- src/sdk/Bit0.CrunchLog.Sdk/Config/Theme.cs
+++ src/sdk/Bit0.CrunchLog.Sdk/Config/Theme.cs
@@ -1,53 +1,43 @@
-﻿using Bit0.CrunchLog.Extensions;
-using Bit0.Registry.Core;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization;
+using Bit0.CrunchLog.Extensions;
+using Bit0.Registry.Core;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
-namespace Bit0.CrunchLog.Config
-{
-    public class Theme : Pack
-    {
-        public Theme(FileInfo themeConfig)
-        {
+namespace Bit0.CrunchLog.Config {
+    public class Theme : Pack {
+        public Theme (FileInfo themeConfig) {
             PackFile = themeConfig;
+
+            TemplateRoot = PackFile.Directory.CombineDirPath ("src");
+            if (!TemplateRoot.Exists) {
+                TemplateRoot = PackFile.Directory;
+            }
         }
 
-        [JsonExtensionData]
-        private readonly IDictionary<String, JToken> _additionalData = new Dictionary<String, JToken>();
-
-        [JsonProperty("outputType")]
+        [JsonProperty ("outputType")]
         public ThemeOutputType OutputType { get; set; } = ThemeOutputType.Html;
 
-        [JsonProperty("output")]
+        [JsonProperty ("output")]
         public ThemeOutput Output { get; set; }
 
-        [JsonProperty("engineType")]
+        [JsonProperty ("engineType")]
         public String EngineType { get; set; }
 
         [JsonIgnore]
         public DirectoryInfo TemplateRoot { get; set; }
 
-        [OnDeserialized]
-        internal void OnDeserializedMethod(StreamingContext context)
-        {
-            var srcKey = (String)_additionalData["sourceDir"][0];
-            TemplateRoot = PackFile.Directory.CombineDirPath(srcKey);
-        }
+        public static Theme Get (FileInfo themeFile, DirectoryInfo outputDir) {
+            var theme = new Theme (themeFile);
 
-        public static Theme Get(FileInfo themeFile, DirectoryInfo outputDir)
-        {
-            var theme = new Theme(themeFile);
-
-            using (var streamReader = themeFile.OpenText())
-            {
-                JsonConvert.PopulateObject(streamReader.ReadToEnd(), theme);
+            using (var streamReader = themeFile.OpenText ()) {
+                JsonConvert.PopulateObject (streamReader.ReadToEnd (), theme);
             }
-            
-            theme.Output.Data = outputDir.CombineDirPath(theme.Output.Data.Name);
+
+            theme.Output.Data = outputDir.CombineDirPath (theme.Output.Data.Name);
 
             return theme;
         }

1c3ac63b16c20240984304aa940d88298cf1b199
b1thunt3r <ishan@jain.se>
2020-11-26 21:39:27 +0000

    Clean up unused imports

--- src/Bit0.CrunchLog.Sdk/Config/Theme.cs
+++ src/Bit0.CrunchLog.Sdk/Config/Theme.cs
@@ -2,9 +2,7 @@
 using Bit0.Registry.Core;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Bit0.CrunchLog.Config
